d1c02fdc26d53765daf392bd9806820aadce5af8
Kresa12 <aurakresa12@gmail.com>
2025-11-15 22:30:12 +0700

    remove PushNotification from CardCreditProcessor and BankTransferProcessor

--- latihan/pengambilandata/bank_transfer_processor.go
+++ latihan/pengambilandata/bank_transfer_processor.go
@@ -8,8 +8,3 @@ func (b *BankTransferProcessor) PaymentProces(amount float32) error {
 	fmt.Printf("Memproses transfer bank sejumlah %.2f\n", amount)
 	return nil
 }
-
-func (b *BankTransferProcessor) PushNotification(email, message string) error {
-	fmt.Printf("Memproses transfer bank sejumlah %.2f\n", email, message)
-	return nil
-}
--- latihan/pengambilandata/credit_card_processor.go
+++ latihan/pengambilandata/credit_card_processor.go
@@ -8,8 +8,3 @@ func (c *CreditCardProcessor) PaymentProces(amount float32) error {
 	fmt.Printf("Memproses pembayaran kartu kredit sejumlah %.2f\n", amount)
 	return nil
 }
-
-func (b *CreditCardProcessor) PushNotification(email, message string) error {
-	fmt.Printf("Memproses transfer bank sejumlah %.2f\n", email, message)
-	return nil
-}
